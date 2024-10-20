c4224c3a7a8f191618b413dfcb9da3d462b24879
Joao Victor Sena <jvsena16@gmail.com>
2024-10-20 12:30:05 -0300

    feat: create wallet

--- app/src/main/java/com/github/jvsena42/floresta/domain/bitcoin/WalletMethods.kt
+++ app/src/main/java/com/github/jvsena42/floresta/domain/bitcoin/WalletMethods.kt
@@ -1,6 +1,12 @@
 package com.github.jvsena42.floresta.domain.bitcoin
 
 import org.bitcoindevkit.Connection
+import org.bitcoindevkit.Descriptor
+import org.bitcoindevkit.DescriptorSecretKey
+import org.bitcoindevkit.KeychainKind
+import org.bitcoindevkit.Mnemonic
+import org.bitcoindevkit.WordCount
+import org.rustbitcoin.bitcoin.Network
 
 class WalletMethods(
     private val dbPath: String,
@@ -19,6 +25,42 @@ class WalletMethods(
     private fun setPathAndConnectDb(path: String) {
         florestaDbPath = "$path/florestaDB_${PERSISTENCE_VERSION}.sqlite3"
         dbConnection = Connection(florestaDbPath)
+    }
+
+    private fun initialize(
+        descriptor: Descriptor,
+        changeDescriptor: Descriptor,
+    ) {
+        wallet = org.bitcoindevkit.Wallet(
+            descriptor,
+            changeDescriptor,
+            Network.SIGNET,
+            dbConnection
+        )
+    }
+
+    fun createWallet() {
+        val mnemonic = Mnemonic(WordCount.WORDS12)
+        val bip32ExtendedRootKey = DescriptorSecretKey(Network.SIGNET, mnemonic, null)
+        val descriptor: Descriptor = Descriptor.newBip84(
+            bip32ExtendedRootKey,
+            KeychainKind.EXTERNAL,
+            Network.SIGNET
+        )
+
+        val changeDescriptor: Descriptor = Descriptor.newBip84(
+            bip32ExtendedRootKey,
+            KeychainKind.INTERNAL,
+            Network.SIGNET
+        )
+
+        initialize(
+            descriptor = descriptor,
+            changeDescriptor = changeDescriptor
+        )
+
+        //TODO SAVE WALLET
+        //TODO SAVE Mnemonic
     }
 
     companion object {
