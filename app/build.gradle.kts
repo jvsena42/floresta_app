8a709716762a5b493b056037c82240a84b2f375c
Joao Victor Sena <jvsena16@gmail.com>
2024-10-22 10:05:13 -0300

    fix: min api level

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.github.jvsena42.floresta"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
