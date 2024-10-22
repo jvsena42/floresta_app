0dd37fa31d260f1b0c730016d27d834f6037269b
Joao Victor Sena <jvsena16@gmail.com>
2024-10-22 11:17:16 -0300

    feat: daemon setup

--- app/src/main/java/com/github/jvsena42/floresta/FlorestaApplication.kt
+++ app/src/main/java/com/github/jvsena42/floresta/FlorestaApplication.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import com.github.jvsena42.floresta.domain.bitcoin.WalletManager
 import com.github.jvsena42.floresta.domain.bitcoin.WalletRepository
 import com.github.jvsena42.floresta.domain.bitcoin.WalletRepositoryImpl
+import com.github.jvsena42.floresta.domain.floresta.FlorestaDaemon
+import com.github.jvsena42.floresta.domain.floresta.FlorestaDaemonImpl
 import com.github.jvsena42.floresta.presentation.MainViewmodel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.core.context.startKoin
@@ -28,13 +30,24 @@ private val presentationModule = module {
 }
 
 private val domainModule = module {
-    single { WalletManager(dbPath = androidApplication().filesDir.toString(), walletRepository = get()) }
+    single {
+        WalletManager(
+            dbPath = androidApplication().filesDir.toString(),
+            walletRepository = get()
+        )
+    }
     single<WalletRepository> {
         WalletRepositoryImpl(
             androidApplication().getSharedPreferences(
                 "wallet",
                 Context.MODE_PRIVATE
             )
+        )
+    }
+    single<FlorestaDaemon> {
+        FlorestaDaemonImpl(
+            datadir = androidApplication().filesDir.toString(),
+            walletRepository = get()
         )
     }
 }
---
+++ app/src/main/java/com/github/jvsena42/floresta/domain/floresta/FlorestaDaemon.kt
@@ -0,0 +1,73 @@
+package com.github.jvsena42.floresta.domain.floresta
+
+import android.util.Log
+import com.florestad.Config
+import com.florestad.Florestad
+import com.github.jvsena42.floresta.domain.bitcoin.WalletRepository
+import org.bitcoindevkit.Descriptor
+import org.rustbitcoin.bitcoin.Network
+import com.florestad.Network as FlorestaNetwork
+import kotlin.let
+
+interface FlorestaDaemon {
+    suspend fun start()
+    suspend fun restart()
+    suspend fun stop()
+}
+
+class FlorestaDaemonImpl (
+    private val datadir: String,
+    private val walletRepository: WalletRepository
+) : FlorestaDaemon {
+
+    var isRunning = false
+    private lateinit var daemon: Florestad
+    override suspend fun start() {
+        Log.d(TAG, "start: ")
+        if (isRunning) {
+            Log.d(TAG, "start: Daemon already running")
+            return
+        }
+        try {
+            val descriptorList = walletRepository.getInitialWalletData().getOrNull()?.let {
+                listOf(
+                    Descriptor(
+                        descriptor = it.descriptor,
+                        Network.SIGNET
+                    ).toStringWithSecret()
+                )
+            }
+            Log.d(TAG, "start: descriptor list: $descriptorList")
+            Log.d(TAG, "start: datadir: $datadir")
+            val config = Config(
+                dataDir = datadir,
+                electrumAddress = ELECTRUM_ADDRESS,
+                network = FlorestaNetwork.SIGNET,
+                walletDescriptor = descriptorList.toString()
+            )
+            daemon = Florestad.fromConfig(config)
+            daemon.start().also { isRunning = true }
+        } catch (e: Exception) {
+            Log.e(TAG, "start error: ",e)
+        }
+    }
+
+    override suspend fun restart() {
+        if (isRunning) {
+            stop()
+            start()
+        } else {
+            start()
+        }
+    }
+
+    override suspend fun stop() {
+        daemon.stop()
+        isRunning = false
+    }
+
+    companion object {
+        const val ELECTRUM_ADDRESS = "127.0.0.1:50001"
+        private const val TAG = "FlorestaDaemonImpl"
+    }
+}
