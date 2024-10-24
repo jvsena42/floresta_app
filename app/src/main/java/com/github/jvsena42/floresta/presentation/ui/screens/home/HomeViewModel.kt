265417d223a4f542e08dc76d96fe504bd29e73a1
Joao Victor Sena <jvsena16@gmail.com>
2024-10-24 18:44:03 -0300

    feat: setup ui WIP

--- app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/HomeViewModel.kt
@@ -2,31 +2,56 @@ package com.github.jvsena42.floresta.presentation.ui.screens.home
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.github.jvsena42.floresta.domain.bitcoin.WalletManager
 import com.github.jvsena42.floresta.domain.bitcoin.WalletRepository
+import com.github.jvsena42.floresta.domain.floresta.FlorestaDaemon
 import com.github.jvsena42.floresta.presentation.util.formatInBtc
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 class HomeViewModel(
     private val walletRepository: WalletRepository,
-    private val walletManager: WalletManager
-) : ViewModel() {
+    private val walletManager: WalletManager,
+    private val florestaDaemon: FlorestaDaemon
+    ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(HomeUIState())
     val uiState = _uiState.asStateFlow()
 
-    private fun setup() {
+    init {
+        syncInLoop()
+    }
+
+    private suspend fun updateUI() {
         if (walletRepository.doesWalletExist()) {
             Log.d(TAG, "setup: Wallet exists")
             val balanceSats = walletManager.getBalance()
-            _uiState.update { it.copy(
-                balanceBTC = balanceSats.formatInBtc(),
-                balanceSats = balanceSats.toString()
-            ) }
+            _uiState.update {
+                it.copy(
+                    balanceBTC = balanceSats.formatInBtc(),
+                    balanceSats = balanceSats.toString()
+                )
+            }
         } else {
             Log.d(TAG, "setup: Wallet does not exists")
+            walletManager.createWallet()
+            florestaDaemon.restart()
+            walletManager.loadWallet()
+        }
+    }
+
+    private fun syncInLoop() {
+        viewModelScope.launch(Dispatchers.IO) {
+            updateUI()
+            walletManager.sync()
+            delay(5.seconds)
+            syncInLoop()
         }
     }
 
