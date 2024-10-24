4385e0fc8ffe3de0757902a35ce224412f557796
Joao Victor Sena <jvsena16@gmail.com>
2024-10-24 15:08:49 -0300

    feat: setup WIP

--- app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/HomeViewModel.kt
@@ -1,11 +1,36 @@
 package com.github.jvsena42.floresta.presentation.ui.screens.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.github.jvsena42.floresta.domain.bitcoin.WalletManager
 import com.github.jvsena42.floresta.domain.bitcoin.WalletRepository
+import com.github.jvsena42.floresta.presentation.util.formatInBtc
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class HomeViewModel(
-    private val walletRepository: WalletRepository
+    private val walletRepository: WalletRepository,
+    private val walletManager: WalletManager
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(HomeUIState())
+    val uiState = _uiState.asStateFlow()
 
+    private fun setup() {
+        if (walletRepository.doesWalletExist()) {
+            Log.d(TAG, "setup: Wallet exists")
+            val balanceSats = walletManager.getBalance()
+            _uiState.update { it.copy(
+                balanceBTC = balanceSats.formatInBtc(),
+                balanceSats = balanceSats.toString()
+            ) }
+        } else {
+            Log.d(TAG, "setup: Wallet does not exists")
+        }
+    }
+
+    companion object {
+        private const val TAG = "HomeViewModel"
+    }
 }
