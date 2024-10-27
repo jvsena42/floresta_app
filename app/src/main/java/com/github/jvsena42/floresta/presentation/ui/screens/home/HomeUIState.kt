0e44c2c978408a736e9449a85b8674fc09c9cb28
Joao Victor Sena <jvsena16@gmail.com>
2024-10-27 17:35:49 -0300

    fix: add stable tag

--- app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/HomeUIState.kt
+++ app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/HomeUIState.kt
@@ -1,5 +1,8 @@
 package com.github.jvsena42.floresta.presentation.ui.screens.home
 
+import androidx.compose.runtime.Stable
+
+@Stable
 data class HomeUIState(
     val balanceBTC: String = "",
     val balanceSats: String = "",
