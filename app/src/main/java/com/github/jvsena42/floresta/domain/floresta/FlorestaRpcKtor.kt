2f08b13726302c6379a60845123c2c8737758bc1
Joao Victor Sena <jvsena16@gmail.com>
2024-10-27 08:27:44 -0300

    feat: ktor client implementation

--- app/src/main/java/com/github/jvsena42/floresta/domain/floresta/FlorestaRpcKtor.kt
+++ app/src/main/java/com/github/jvsena42/floresta/domain/floresta/FlorestaRpcKtor.kt
@@ -9,23 +9,25 @@ import io.ktor.network.sockets.openWriteChannel
 import io.ktor.utils.io.ByteWriteChannel
 import io.ktor.utils.io.readUTF8Line
 import io.ktor.utils.io.writeStringUtf8
+import kotlinx.coroutines.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.time.Duration.Companion.seconds
 
 class FlorestaRpcKtor {
 
     private val ioScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-    val selectorManager = SelectorManager(Dispatchers.IO)
-    lateinit var socket: Socket
-    lateinit var sendChannel: ByteWriteChannel
-
+    private var selectorManager: SelectorManager? = null
+    private var socket: Socket? = null
+    private var sendChannel: ByteWriteChannel? = null
     private val _response = MutableStateFlow("")
     val response = _response.asStateFlow()
 
@@ -35,28 +37,58 @@ class FlorestaRpcKtor {
 
     suspend fun getBlockchainInfo() {
         Log.d(TAG, "getBlockchainInfo: ")
-        val arguments = JSONArray()
 
+        val arguments = JSONArray()
         val request = buildJsonRpcRequest(method = "getblockchaininfo", params = arguments)
         sendMessage(request)
     }
 
     private fun startSocket() = ioScope.launch {
-        socket = aSocket(selectorManager).tcp().connect(ELECTRUM_ADDRESS, RCP_PORT)
+        selectorManager = SelectorManager(Dispatchers.IO)
 
-        val receiveChannel = socket.openReadChannel()
-        sendChannel = socket.openWriteChannel(autoFlush = true)
+        while (isActive) {
+            try {
+                Log.d(TAG, "Connecting to socket...")
+                socket = aSocket(selectorManager!!).tcp().connect(ELECTRUM_ADDRESS, RCP_PORT)
+                sendChannel = socket!!.openWriteChannel(autoFlush = true)
+                val receiveChannel = socket!!.openReadChannel()
 
-        while (true) {
-            val response = receiveChannel.readUTF8Line()
-            Log.d(TAG, "response: $response")
-            _response.value = response.toString()
+                Log.d(TAG, "Socket connected successfully.")
+
+                // Start reading responses
+                while (isActive) {
+                    val response = receiveChannel.readUTF8Line()
+                    if (response == null) {
+                        Log.e(TAG, "Connection closed by server.")
+                        break
+                    }
+                    Log.d(TAG, "Received response: $response")
+                    _response.value = response
+                }
+
+            } catch (e: Exception) {
+                Log.e(TAG, "Error in socket connection: ", e)
+                delay(2.seconds)  // Retry connection after delay
+            } finally {
+                closeSocket()
+            }
         }
     }
 
-    private fun close() = ioScope.launch {
-        socket.close()
-        selectorManager.close()
+    private fun closeSocket() {
+        Log.d(TAG, "Closing socket...")
+        socket?.close()
+        selectorManager?.close()
+        sendChannel = null
+        socket = null
+        selectorManager = null
+    }
+
+    private fun close() {
+        ioScope.launch {
+            closeSocket()
+            ioScope.cancel()
+        }
     }
 
     private fun buildJsonRpcRequest(
@@ -72,8 +104,22 @@ class FlorestaRpcKtor {
     }
 
     private suspend fun sendMessage(message: String) {
-        Log.d(TAG, "sendMessage: $message")
-        sendChannel.writeStringUtf8(message)
+        Log.d(TAG, "Sending message: $message")
+        if ((socket == null) || (sendChannel == null)) {
+            delay(0.5.seconds)
+        }
+
+        try {
+            if (sendChannel == null) {
+                Log.e(TAG, "Socket is not connected.")
+                throw IllegalStateException("Socket is not connected.")
+            }
+            sendChannel!!.writeStringUtf8(message)
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to send message: ", e)
+            Log.d(TAG, "sendMessage: Attempt to restart the socket connection")
+            startSocket()
+        }
     }
 
     private companion object {
