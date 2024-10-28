82c52c7f4e57c72a22af95e0f0cfe11923797b7a
Joao Victor Sena <jvsena16@gmail.com>
2024-10-28 19:08:59 -0300

    feat: transaction font weight

--- app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/components/TransactionItem.kt
+++ app/src/main/java/com/github/jvsena42/floresta/presentation/ui/screens/home/components/TransactionItem.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,6 +55,7 @@ fun TransactionItem(
                 title,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .weight(1f)
                     .padding(end = 32.dp)
